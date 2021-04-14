673c8aab2740e13a74d969d95492c25f5d901399
lenovo <lenovo@DESKTOP-29UMFUO>
2021-04-14 16:37:03 +0900

    log 추가

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,5 +1,12 @@
 package test;
 
+import org.apache.log4j.Logger;
+
 public class Main {
 
+	private static Logger logger = Logger.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		logger.debug("test");
+	}
 }
